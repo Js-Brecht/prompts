5b9d95d36f9e84a6ab772ed35fdb4bc316913a2c
Jeremy Albright <jeremy.albright@manaollc.com>
2019-11-08 23:21:22 -1000

    Prompt generation test for debugging

---
+++ test/textPrompt.js
@@ -0,0 +1,15 @@
+const prompts = require('../lib');
+
+prompts([{
+  name: 'textPrompt',
+  type: 'text',
+  message: 'Please enter some text',
+  initial: 'Some initial text',
+  hint: 'Testing hint values for text prompt',
+  validate: (answer) => {
+    if (!/^[a-zA-Z]+$/.test(answer)) {
+      return 'Test alpha-only error message, random asldfjasgklhasgiolwrfgkdsgnsk';
+    }
+    return true;
+  },
+}]).then(console.log);
