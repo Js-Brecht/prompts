83950d7174ababcf34036845bd0f803902fd91a4
Jeremy Albright <myrlin1@manaollc.com>
2019-11-14 10:10:42 -1000

    Implementation of `renderer` for text Prompt

---
+++ test/textPrompt.js
@@ -0,0 +1,62 @@
+const prompts = require('../dist');
+
+prompts([{
+  name: 'textEmoji',
+  type: 'text',
+  message: 'Emoji text prompt',
+  hint: 'This prompt should display Emojis as input mask',
+  style: 'emoji',
+  validate: (answer) => {
+    if (!/^[a-zA-Z-]+$/.test(answer)) {
+      return 'Test kebab-style-only error message, random asldfjasgklhasgiolwrfgkdsgnsk';
+    }
+    return true;
+  },
+}, {
+  name: 'textInvisible',
+  type: 'text',
+  message: 'Password text prompt',
+  hint: 'This prompt should display asterisks as input mask',
+  style: 'password',
+  validate: (answer) => {
+    if (!/^[a-zA-Z-]+$/.test(answer)) {
+      return 'Test kebab-style-only error message, random asldfjasgklhasgiolwrfgkdsgnsk';
+    }
+    return true;
+  },
+}, {
+  name: 'textOnePrompt',
+  type: 'text',
+  message: 'textOnePrompt',
+  initial: 'one-line-initial-text',
+  hint: 'Testing hint values for text prompt',
+  validate: (answer) => {
+    if (!/^[a-zA-Z-]+$/.test(answer)) {
+      return 'Test kebab-style-only error message, random asldfjasgklhasgiolwrfgkdsgnsk';
+    }
+    return true;
+  },
+}, {
+  name: 'textTwoPrompt',
+  type: 'text',
+  message: 'textTwoPrompt\nTwo line prompt!',
+  initial: 'two-line-initial-text',
+  hint: 'Testing hint values for two line text prompt',
+  validate: (answer) => {
+    if (!/^[a-zA-Z-]+$/.test(answer)) {
+      return 'Test kebab-style-only error message, random asldfjasgklhasgiolwrfgkdsgnsk';
+    }
+    return true;
+  },
+}, {
+  name: 'textTwoNoHint',
+  type: 'text',
+  message: 'No Hint Text Prompt',
+  initial: 'no-hint-initial-text',
+  validate: (answer) => {
+    if (!/^[a-zA-Z-]+$/.test(answer)) {
+      return 'Test kebab-style-only error message, random asldfjasgklhasgiolwrfgkdsgnsk';
+    }
+    return true;
+  },
+}]).then(console.log);
